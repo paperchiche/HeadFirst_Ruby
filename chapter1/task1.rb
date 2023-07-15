193649ffaa4c23d825e3a14a9ad70a7f5a641f86
paperchiche <20artyom02@mail.ru>
2023-07-15 13:04:36 +0300

    Счётчик количества попыток. Игра готова

--- chapter1/task1.rb
+++ chapter1/task1.rb
@@ -6,7 +6,6 @@ name = input.chomp
 
 p("Welcome, #{name}!")
 
-# Сохранение случайного числа.
 puts "I've got a random number between 1 and 100."
 puts "Can you guess it?"
 target = rand(100) + 1
@@ -14,18 +13,26 @@ target = rand(100) + 1
 num_guesses = 0
 guessed_it = false
 
-puts "You've got #{10 - num_guesses} guesses left."
-print "Make a guess: "
-guess = gets.to_i
+until num_guesses == 10 || guessed_it
 
-# Сравнение введенного числа с загаданным
-# и вывод соответствующего сообщения.
-if guess < target
-  puts "Oops. Your guess was LOW."
-elsif guess > target
-  puts "Oops. Your guess was HIGH."
-elsif guess == target
-  puts "Good job, #{name}!"
-  puts "You guessed my number in #{num_guesses} guesses!"
-  guessed_it = true
+  puts "You've got #{10 - num_guesses} guesses left."
+  print "Make a guess: "
+  guess = gets.to_i
+
+  num_guesses += 1
+
+  if guess < target
+    puts "Oops. Your guess was LOW."
+  elsif guess > target
+    puts "Oops. Your guess was HIGH."
+  elsif guess == target
+    puts "Good job, #{name}!"
+    puts "You guessed my number in #{num_guesses} guesses!"
+    guessed_it = true
+  end
+
+end
+
+unless guessed_it
+  puts "Sorry. You didn't get my number. (It was #{target}.)"
 end
