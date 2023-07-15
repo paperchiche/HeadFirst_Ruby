4e50df9ce0d2911b7eba4da992ee844abd43ebb3
paperchiche <20artyom02@mail.ru>
2023-07-15 12:27:28 +0300

    Добавлено генерирование случайного числа от 1 до 100

--- chapter1/task1.rb
+++ chapter1/task1.rb
@@ -5,3 +5,8 @@ input = gets
 name = input.chomp
 
 p("Welcome, #{name}!")
+
+# Сохранение случайного числа.
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
