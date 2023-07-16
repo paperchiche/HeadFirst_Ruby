5e301d5e4b948ed0bc05d0e82dada21241e8eb11
paperchiche <20artyom02@mail.ru>
2023-07-16 14:12:14 +0300

    Добавление переменных экземпляра

--- chapter2/animals.rb
+++ chapter2/animals.rb
@@ -1,48 +1,24 @@
-class Bird
-  def talk
-    puts "Chirp! Chirp!"
-  end
-  def move(destination)
-    puts "Flying to the #{destination}."
-  end
-end
-
 class Dog
-  def talk
+  def make_up_name
+    @name = "Sandy"
+  end
+  def talk(name)
     puts "Bark!"
   end
   def move(destination)
-    puts "Running to the #{destination}."
+  puts "#{@name} runs to the #{destination}."
+  end
+  def make_up_age
+    @age = 5
+  end
+  def report_age
+    puts "#{@name} is #{@age} years old."
   end
 end
 
-class Cat
-  def talk
-    puts "Meow!"
-  end
-  def move(destination)
-    puts "Running to the #{destination}."
-  end
-end
 
-class Lion
-  def talk
-    puts "R-r-r-r!"
-  end
-  def move(destination)
-    puts("Running to the #{destination}.")
-  end
-end
-
-bird = Bird.new
 dog = Dog.new
-cat = Cat.new
-lion = Lion.new
-
-
-bird.move("tree")
-dog.talk
-bird.talk
-cat.move("house")
-lion.talk
-lion.move("forest")
+dog.make_up_name
+dog.move("yard")
+dog.make_up_age
+dog.report_age
