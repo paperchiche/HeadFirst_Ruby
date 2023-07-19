adb3032acdd6c12d7db3503e74281ebca4c3710d
paperchiche <20artyom02@mail.ru>
2023-07-19 15:52:31 +0300

    Устранение повторений кода при помощи each и блоков

--- chapter5/prices.rb
+++ chapter5/prices.rb
@@ -1,37 +1,30 @@
 def total(prices)
   amount = 0
-  index = 0
-  while index < prices.length
-    amount += prices[index]
-    index += 1
+  prices.each do |price|
+    amount += price
   end
   amount
 end
-
-prices = [3.99, 25.00, 8.99]
-puts format("Total: %.2f", total(prices))
 
 
 def refund(prices)
   amount = 0
-  index = 0
-  while index < prices.length
-    amount -= prices[index]
-    index += 1
+  prices.each do |price|
+    amount -= price
   end
   amount
 end
 
-puts format("Debited amount: %.2f", refund(prices))
-
 
 def show_discounts(prices)
-  index = 0
-  while index < prices.length
-    amount_off = prices[index] / 3.0
+  prices.each do |price|
+    amount_off = price / 3.0
     puts format("Your discount: $%.2f", amount_off)
-    index += 1
   end
 end
 
+
+prices = [3.99, 25.00, 8.99]
+puts format("Total: %.2f", total(prices))
+puts format("Debited amount: %.2f", refund(prices))
 show_discounts(prices)
