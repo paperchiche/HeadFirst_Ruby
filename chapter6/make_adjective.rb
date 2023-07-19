85397c63b0d70c0f07b83b413bf939a19c6dac1e
paperchiche <20artyom02@mail.ru>
2023-07-19 21:28:20 +0300

    Использование метода find_all

--- chapter6/make_adjective.rb
+++ chapter6/make_adjective.rb
@@ -4,12 +4,5 @@ File.open("C:/GitHub/HeadFirst_Ruby/HeadFirst_Ruby/chapter6/reviews.txt") do |re
   lines = review_file.readlines
 end
 
-relevant_lines = []
-
-lines.each do |line|
-  if line.include?("Truncated")
-    relevant_lines << line
-  end
-end
-
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 puts relevant_lines
