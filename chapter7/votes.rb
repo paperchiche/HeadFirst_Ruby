6f1a6653dd9dacb2792ac18f859124868a60a74e
paperchiche <20artyom02@mail.ru>
2023-07-20 17:02:04 +0300

    Подсчет голосов с помощью хеша

--- chapter7/votes.rb
+++ chapter7/votes.rb
@@ -3,4 +3,15 @@ File.open("C:/GitHub/HeadFirst_Ruby/HeadFirst_Ruby/chapter7/votes.txt") do |file
   lines = file.readlines
 end
 
-puts lines
+votes = {}
+
+lines.each  do |line|
+  name = line.chomp
+  if votes[name] != nil
+    votes[name] += 1
+  else
+    votes[name] = 1
+  end
+end
+
+p votes
