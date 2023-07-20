04bfddbc899f904138425fe77321c092137c3bc7
paperchiche <20artyom02@mail.ru>
2023-07-20 17:53:01 +0300

    Использование ключевых слов в аргументах

--- chapter7/candidate.rb
+++ chapter7/candidate.rb
@@ -1,31 +1,14 @@
 class Candidate
   attr_accessor :name, :age, :occupation, :hobby, :birthplace
 
-  def initialize(name, options)
+  def initialize(name, age:, occupation:, hobby: nil, birthplace: "Sleepy Creek")
     self.name = name
-    self.age = options[:age]
-    self.occupation = options[:occupation]
-    self.hobby = options[:hobby]
-    self.birthplace = options[:birthplace]
+    self.age = age
+    self.occupation = occupation
+    self.hobby = hobby
+    self.birthplace = birthplace
   end
 end
 
-def print_summary(candidate)
-  puts "Candidate: #{candidate.name}"
-  puts "Age: #{candidate.age}"
-  puts "Occupation: #{candidate.occupation}"
-  puts "Hobby: #{candidate.hobby}"
-  puts "Birthplace: #{candidate.birthplace}"
-end
 
-candidate = Candidate.new("Amy Nguyen",
-                          {:age => 37, :occupation => "Engineer", :hobby => "Lacrosse", :birthplace => "Seattle"})
-p candidate
-
-candidate = Candidate.new("Carl Barnes",
-                          {:age => 49, :occupation => "Engineer", :birthplace => "Seattle"})
-p candidate
-
-candidate = Candidate.new("Amy Nguyen",
-                          { :occupation => "Engineer", :age => 37,  :birthplace => "Seattle", :hobby => "Lacrosse"})
-p candidate
+p Candidate.new("Carl Barnes", age: 49, occupation: "Attorney")
