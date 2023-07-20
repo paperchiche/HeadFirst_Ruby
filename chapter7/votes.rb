9fa36596e19af96e825ac142a52f990a729bcbe5
paperchiche <20artyom02@mail.ru>
2023-07-20 17:09:31 +0300

    Избавление от if

--- chapter7/votes.rb
+++ chapter7/votes.rb
@@ -3,15 +3,11 @@ File.open("C:/GitHub/HeadFirst_Ruby/HeadFirst_Ruby/chapter7/votes.txt") do |file
   lines = file.readlines
 end
 
-votes = {}
+votes = Hash.new(0)
 
 lines.each  do |line|
   name = line.chomp
-  if votes[name] != nil
-    votes[name] += 1
-  else
-    votes[name] = 1
-  end
+  votes[name] += 1
 end
 
 p votes
