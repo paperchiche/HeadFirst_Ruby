a5ef156bf8159b4c6adcf35e891380685146f371
paperchiche <20artyom02@mail.ru>
2023-07-21 22:44:36 +0300

    Блоки по умолчанию для хеша

--- chapter8/hash.rb
+++ chapter8/hash.rb
@@ -2,9 +2,15 @@ class CelestialBody
   attr_accessor :type, :name
 end
 
-default_body = CelestialBody.new
-default_body.type = 'planet'
-bodies = Hash.new(default_body)
+bodies = Hash.new do |hash, key|
+  body = CelestialBody.new
+  body.type = "planet"
+  hash[key] = body
+end
 
 bodies['Mars'].name = 'Mars'
-p bodies['Mars']
+bodies['Europa'].name = 'Europa'
+bodies['Europa'].type = 'moon'
+bodies['Venus'].name = 'Venus'
+
+p bodies
