d7648db800255d1f6cf740813c56af6210d465f5
paperchiche <20artyom02@mail.ru>
2023-07-21 22:27:21 +0300

    Добавление объекта в хеш

---
+++ chapter8/hash.rb
@@ -0,0 +1,10 @@
+class CelestialBody
+  attr_accessor :type, :name
+end
+
+default_body = CelestialBody.new
+default_body.type = 'planet'
+bodies = Hash.new(default_body)
+
+bodies['Mars'].name = 'Mars'
+p bodies['Mars']
