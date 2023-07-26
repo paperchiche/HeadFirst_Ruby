ae1a04271dfbfbbfe4582489d7d90562fa544b2e
paperchiche <20artyom02@mail.ru>
2023-07-27 01:15:21 +0300

    Создание класса Photo

--- chapter9/media.rb
+++ chapter9/media.rb
@@ -33,10 +33,19 @@ class Song < Clip
   attr_accessor :beats_per_minute
 end
 
+class Photo
+  include AcceptsComments
+  def initialize
+    @format = 'JPEG'
+  end
+end
+
 video = Video.new
 video.add_comment("Cool slow motion effect!")
 video.add_comment("Weird ending.")
 song = Song.new
 song.add_comment("Avesome beat.")
+photo = Photo.new
+photo.add_comment("Beautiful colors.")
 
-p video.comments, song.comments
+p video.comments, song.comments, photo.comments
