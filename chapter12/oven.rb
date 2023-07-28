c23ebdc602502424c8044be5f81901cab6afc4f4
paperchiche <20artyom02@mail.ru>
2023-07-28 22:15:26 +0300

    Глава 12. Класс SmallOven

---
+++ chapter12/oven.rb
@@ -0,0 +1,15 @@
+class SmallOven
+
+  attr_accessor :contents
+
+  def turn_on
+    puts "Turning oven on."
+    @state = "on"
+  end
+
+  def turn_off
+    puts "Turning oven off."
+    @state = "off"
+  end
+
+end
