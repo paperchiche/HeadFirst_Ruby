e74c71c93ffece0ac1d28668e34fcb2f49e80604
paperchiche <20artyom02@mail.ru>
2023-08-01 01:30:25 +0300

    Исправление ошибки в классе ListWithCommas

--- chapter13/lib/list_with_commas.rb
+++ chapter13/lib/list_with_commas.rb
@@ -1,8 +1,19 @@
 class ListWithCommas
   attr_accessor :items
   def join
+    if items.length == 2
+      return "#{items[0]} and #{items[1]}"
+    end
     last_item = "and #{items.last}"
     other_items =  items.slice(0, items.length - 1).join(', ')
     "#{other_items}, #{last_item}"
   end
 end
+
+
+two_subjects = ListWithCommas.new
+two_subjects.items = ['my parents', 'a rodeo clown']
+puts "a photo of #{two_subjects.join}"
+three_subjects = ListWithCommas.new
+three_subjects.items = ['my parents', 'a rodeo clown', 'a prize bull']
+puts "a photo of #{three_subjects.join}"
