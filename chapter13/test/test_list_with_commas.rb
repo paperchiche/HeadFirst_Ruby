2866d90eb1f67f7d74e6e6cf95cd159266ceb6d9
paperchiche <20artyom02@mail.ru>
2023-08-01 05:13:42 +0300

    Проведение успешного теста в классе ListWithCommas

--- chapter13/test/test_list_with_commas.rb
+++ chapter13/test/test_list_with_commas.rb
@@ -3,16 +3,22 @@ require 'list_with_commas'
 
 class TestListWithCommas < Minitest::Test
 
+  def test_it_prints_one_word_alone
+    list = ListWithCommas.new
+    list.items = ['apple']
+    assert_equal('apple', list.join)
+  end
+
   def test_it_joins_two_words_with_and
     list = ListWithCommas.new
     list.items = ['apple', 'orange']
-    asset('apple and orange' == list.join)
+    assert_equal('apple and orange', list.join)
   end
 
   def test_it_joins_three_words_with_commas
     list = ListWithCommas.new
     list.items = ['apple', 'orange', 'pear']
-    asset('apple, orange and pear' == list.join)
+    assert_equal('apple, orange and pear', list.join)
   end
 
 end
