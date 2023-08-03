e4afc0cf407d8d1e04cb10693b0be593b27ccf56
paperchiche <20artyom02@mail.ru>
2023-08-03 03:34:22 +0300

    Страница HTML заполнена списком объектов фильмов

--- chapter14-15/movies/app.rb
+++ chapter14-15/movies/app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'movie'
+
 
 get('/movies') do
+  @movies = []
+  @movies[0] = Movie.new
+  @movies[0] = "Jaws"
+  @movies[1] = Movie.new
+  @movies[1] = "Alien"
+  @movies[2] = Movie.new
+  @movies[2] = "Terminator 2"
   erb :index
 end
